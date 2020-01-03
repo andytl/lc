ee39fb3c2e20159b3937048a8dad30fc7528f78d
Andy Lam <lam.andy.tr@outlook.com>
2020-01-02 19:37:19 -0800

    mergeKLists + medianFinder

---
+++ problems/medianFinder.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <queue>
+#include <functional>
+#include "gtest/gtest.h"
+
+namespace medianFinder {
+    using namespace std;
+
+    class MedianFinder {
+        priority_queue<int, vector<int>, less<int>> smaller;
+        priority_queue<int, vector<int>, greater<int>> larger;
+    public:
+        /** initialize your data structure here. */
+        MedianFinder() {
+            
+        }
+        
+        void addNum(int num) {
+            if (smaller.empty() || num >= smaller.top()) {
+                larger.push(num);
+            } else {
+                smaller.push(num);
+            }
+            if (smaller.size() > larger.size()) {
+                larger.push(smaller.top());
+                smaller.pop();
+            } else if (smaller.size() + 1 < larger.size()) {
+                smaller.push(larger.top());
+                larger.pop();
+            }
+        }
+        
+        double findMedian() {
+            if (larger.size() > smaller.size()) {
+                return static_cast<double>(larger.top());
+            } else {
+                return static_cast<double>(larger.top() + smaller.top()) / 2;
+            }
+        }
+    };
+
+
+    TEST(medianFinder, T1) {
+        MedianFinder mf;
+        mf.addNum(1);
+        mf.addNum(2);
+        EXPECT_EQ(mf.findMedian(), 1.5);
+        mf.addNum(3);
+        EXPECT_EQ(mf.findMedian(), 2);
+    }
+    TEST(medianFinder, T2) {
+        MedianFinder mf;
+        mf.addNum(2);
+        mf.addNum(3);
+        mf.addNum(4);
+        EXPECT_EQ(mf.findMedian(), 3);
+    }
+    TEST(medianFinder, T3) {
+        MedianFinder mf;
+        mf.addNum(2);
+        mf.addNum(3);
+        EXPECT_EQ(mf.findMedian(), 2.5);
+    }
+}
---
+++ problems/mergeKLists.cpp
@@ -0,0 +1,94 @@
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <functional>
+#include "gtest/gtest.h"
+
+namespace mergeKLists {
+    using namespace std;
+
+    // Check for leaks... assume single thread.
+    int leakCount = 0;
+    struct ListNode {
+        int val;
+        ListNode *next;
+        ListNode(int x) : val(x), next(NULL) {leakCount++;}
+        ~ListNode() {leakCount--;}
+    };
+
+    list<int> convertToList(ListNode *node) {
+        list<int> ret;
+        while (node != nullptr) {
+            ListNode *cNode = node;
+            node = node->next;
+            ret.push_back(cNode->val);
+            delete cNode;
+        }
+        return ret;
+    }
+    ListNode* convertFromList(list<int> nodes) {
+        ListNode* ret = nullptr;
+        ListNode* cur = nullptr;
+        for (auto &n : nodes) {
+            if (ret == nullptr) {
+                ret = cur = new ListNode(n);
+            } else {
+                cur->next = new ListNode(n);
+                cur = cur->next;
+            }
+        }
+        return ret;
+    }
+
+    class Solution {
+        struct LNComp {
+            bool operator()(ListNode* a, ListNode* b) {
+                return a->val > b->val;
+            }
+        };
+    public:
+        ListNode* mergeKLists(vector<ListNode*>& lists) {
+            priority_queue<ListNode*, vector<ListNode*>, LNComp> listQ;
+            ListNode* ret = nullptr;
+            ListNode* cur = nullptr;
+            for (auto list : lists) {
+                if (list != nullptr) {
+                    listQ.push(list);
+                }
+            }
+            while (!listQ.empty()) {
+                ListNode* next = listQ.top();
+                listQ.pop();
+                if (next->next) {
+                    listQ.push(next->next);
+                }
+
+                if (ret == nullptr) {
+                    ret = cur = next;
+                } else {
+                    cur->next = next;
+                    cur = cur->next;
+                }
+                cur->next = nullptr;
+            }
+            return ret;
+        }
+    };
+    Solution s;
+
+    TEST(mergeKLists, T1) {
+        auto result =
+            convertToList(
+                s.mergeKLists(
+                    vector<ListNode*> {
+                        convertFromList(list<int> {1, 4, 5}),
+                        convertFromList(list<int> {1, 3, 4}),
+                        convertFromList(list<int> {2, 6})
+                    }
+                )
+            );
+
+        EXPECT_EQ(result, (list<int> {1, 1, 2, 3, 4, 4, 5, 6}));
+        EXPECT_EQ(leakCount, 0);
+    }
+}
